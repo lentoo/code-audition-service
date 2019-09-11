5478a4f65b927967a2d6e594962837109594cc72
lentoo <729533020@qq.com>
2019-09-11 21:04:51 +0800

    test travis

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ pwd
 
 ls
 
-docker-compose stop
-docker-compose rm -f
-docker rmi egg-code-audition_egg-code-audition
-docker-compose up -d
+# docker-compose stop
+# docker-compose rm -f
+# docker rmi egg-code-audition_egg-code-audition
+# docker-compose up -d
 
